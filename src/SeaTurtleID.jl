1fe2526b92eb3ad1c6ee0f25c4688e46f23a4b79
roviralta <sergirovisu74@gmail.com>
2023-01-26 20:31:50 +0100

    added data directory and loadImages

--- src/SeaTurtleID.jl
+++ src/SeaTurtleID.jl
@@ -1,15 +1,15 @@
 module SeaTurtleID
 
-using  JSON, DataFrames
+using  JSON, DataFrames, Images, Flux
 
 # Write your package code here.
-export loadDataset
+export loadDataset, loadImages
 
 
 function loadDataset()
 
     # Define the path to the JSON file
-    file_path = "../annotations.json"
+    file_path = "../data/annotations.json"
 
     # Read the JSON file
     data = JSON.parsefile(file_path)
@@ -18,6 +18,26 @@ function loadDataset()
     column_data = data["images"]
 
     dataset = DataFrame(column_data)
+
+    return dataset
+
+end
+
+function loadImages()
+
+    dataset = loadDataset()
+    images = dataset[:, "path"]
+    return images
+end
+
+function detectFaces()
+
+    images = loadImages()
+    turtlesID = collect(1:400)
+
+    img_size = (224, 224)
+    imgs = [Flux.Data.DataLoader(load(img), img_size) for img in images]
+
 
 end
 
