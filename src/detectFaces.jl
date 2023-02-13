fdf86110def61383b05f80188ce33233831d3c12
roviralta <sergirovisu74@gmail.com>
2023-02-13 22:10:45 +0100

    fixing errors and gradient, start of adam algorithm

--- src/detectFaces.jl
+++ src/detectFaces.jl
@@ -4,7 +4,7 @@ using Statistics
 using Plots
 
 # Write your package code here.
-export detectFaces, loss, accuracy
+export detectFaces, loss, accuracy, Adam, calculate_gradient, train_model
 
 """
     detecFaces()
@@ -46,14 +46,42 @@ function loss(X, y)
     return loss
 end
 
-function accuracy(X, y)
-    return mean(onecold(model(x)) .== onecold(y))
+function accuracy(X, y, classes)
+    return mean(onecold(model(X), classes) .== onecold(y, classes))
 end
 
-function SGD_or_ADAM(model, learning_rate)
-    nothing
+function Adam(model; learning_rate = 0.001, epochs=10, decay_rate1=0.9, decay_rate2=0.999, ε=1e-8)
+
+    params = params(model)
+    m = [zeros(size(p)) for p in params]        # moving average
+    v = [zeros(size(p)) for p in params]        # gradient average
+
+    for _ in 1:epochs
+
+        g = calculate_gradient(() -> loss(model(X), y), params)
+
+    end
+
 end
 
-function train(epochs)
+function calculate_gradient(f, params)
+
+    g = []
+    for par in params
+        push!(g, similar(par))
+        for i in eachindex(par)
+            par[i] += 1e-8
+            f_plus = f()
+            par[i] -= 2e-8
+            f_minus = f()
+            g[end][i] = (f_plus - f_minus) / (2e-8)         #   finite difference formula  f (x + b) − f (x + a)
+            par[i] += 1e-8
+        end
+    end
+
+    return g
+end
+
+function train_model(epochs)
     nothing
 end
